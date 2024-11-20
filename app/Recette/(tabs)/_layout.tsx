7de2825a6d1f76ada1a9683b61092d77970fac01
1u1ucas <lucasleveque17@gmail.com>
2024-11-20 13:07:55 +0100

    --

--- app/Recette/(tabs)/_layout.tsx
+++ app/Recette/(tabs)/_layout.tsx
@@ -1,11 +1,20 @@
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function MealsLayout() {
   return (
  
       <Tabs>
-        <Tabs.Screen name="search" options={{title: 'recettes', headerShown: false}}  />
-        <Tabs.Screen name="random" options={{title: 'Random', headerShown: false}}  />
+        <Tabs.Screen name="search" options={{title: 'recettes', headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search" color={color} size={size} />
+          ),
+        }}  />
+        <Tabs.Screen name="random" options={{title: 'Random', headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="shuffle" color={color} size={size} />
+          ),
+        }}  />
       </Tabs>
 
   )
